8c7a52128b9db9a4fad388275a74e2d5f0d830c8
Fahim <fahimyadgir07@gmail.com>
2026-04-19 19:39:07 +0530

    add while loop for better perfomance like the program not stop if the program end wherever user enter stop in that time it will be stop

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-year = int(input("Enter year: "))
 
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print("Leap Year")
-else:
-    print("Not Leap Year")
+while True:
+    user =input("Enter year: (or end for stop)")
+    if user.lower() == 'stop':
+        break
+
+    year = int(user)
+    
+    if (year %4==0 and year %100 !=0 )or (year %400==0):
+        print("Leap Year")
+    else:
+        print("Not Leap Year")
