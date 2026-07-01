6d21c350f8d86bb20590ab0728e4d9d13baea001
Fahim <fahimyadgir07@gmail.com>
2026-07-01 10:52:08 +0530

    perform some practice in python

---
+++ practice.py
@@ -0,0 +1,24 @@
+# num = 123456
+
+# rev = 0
+
+# while num > 0:
+#     digit = num % 10
+#     rev = rev * 10 + digit
+#     num = num//10
+
+# print(rev)
+
+
+# num = '123456789'
+
+# print(num[::-1])
+
+string = 'fahim'
+
+rev = ''
+
+for i in string:
+    rev = i + rev
+
+print(rev)
