ee731cfa25fcb047b0afd237d5a0674d5147b159
Fahim <fahimyadgir07@gmail.com>
2026-07-01 10:55:01 +0530

    using for loop

--- practice.py
+++ practice.py
@@ -14,11 +14,22 @@
 
 # print(num[::-1])
 
-string = 'fahim'
+# string = 'fahim'
 
-rev = ''
+# rev = ''
 
-for i in string:
-    rev = i + rev
+# for i in string:
+#     rev = i + rev
+
+# print(rev)
+
+n = 123456
+
+rev = 0
+
+for i in range(len(str(n))):
+    digit = n % 10
+    rev = rev * 10 + digit
+    n = n // 10
 
 print(rev)
